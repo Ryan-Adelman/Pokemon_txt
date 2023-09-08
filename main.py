198ad2643beef84fefdd8c345e9eba0dc02ed5ae
unknown <boopliana4@gmail.com>
2023-09-08 00:37:51 -0700

    Actually adding in the stuff for getting the trainer's information

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ pokemon
 items
 
 """
+import trainer.py
+import pokemon.py
+import items.py
+
+
 def start():
-    pass
+    #first we need to get all the information to setup the main player and their rivala
+    mp_gender = input("Welcome to the world of pokemon! Tell me, are you a Boy or a Girl?")
+    while mp_gender.lower != 'boy' or mp_gender.lower != 'girl':
+        mp_gender = input("Sorry what was that? Are you a Boy or a Girl")
+    if(mp_gender.lower == 'boy'):
+        mp_name = input("What is your name young lad?")
+    elif(mp_gender.lower == 'girl'):
+        mp_name = input("What is your name young lass?")
+    else:
+        mp_name = input("What is your name?")
+    
+    
+
+
 
